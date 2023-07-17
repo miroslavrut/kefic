8c9e08e572cef2ee0fa42679f3fca995b1c718e5
Miroslav Rutesic <miroslavr@protonmail.com>
2023-07-17 20:52:06 +0200

    Changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,24 +1,26 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   icons: {
     icon: {
-      url: "favicon.png",
-      type: "image/png"
+      url: "logo-circle.png",
+      type: "image/png",
     },
-    shortcut: { url: "/favicon.png", type: "image/png"}
+    shortcut: { url: "/logo-circle.png", type: "image/png" },
   },
-  title: 'Meetbusters',
-  description: '',
-}
+  title: "Meetbusters",
+  description: "",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className} id="root">{children}</body>
+      <body className={inter.className} id="root">
+        {children}
+      </body>
     </html>
-  )
+  );
 }
